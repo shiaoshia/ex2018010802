914d64763b2ea77d28a76a9481a96bddd71c43de
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-08 13:17:28 +0800

    SimpleAdapter加圖片

--- app/src/main/java/tw/com/shiaoshia/ex2018010802/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018010802/MainActivity.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Map<String, String>> mylist = new ArrayList();
+    ArrayList<Map<String, Object>> mylist = new ArrayList();
     ListView lv;
 
     @Override
@@ -19,22 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lv = (ListView)findViewById(R.id.listView);
 
-        HashMap<String,String> m1 = new HashMap<>();
+        HashMap<String,Object> m1 = new HashMap<>();
         m1.put("city","台北");
         m1.put("code","02");
+        m1.put("img",R.drawable.images001);
         mylist.add(m1);
-        HashMap<String,String> m2 = new HashMap<>();
+        HashMap<String,Object> m2 = new HashMap<>();
         m2.put("city","台中");
         m2.put("code","04");
+        m2.put("img",R.drawable.images002);
         mylist.add(m2);
-        HashMap<String,String> m3 = new HashMap<>();
+        HashMap<String,Object> m3 = new HashMap<>();
         m3.put("city","台南");
         m3.put("code","06");
+        m3.put("img",R.drawable.images003);
         mylist.add(m3);
 
         SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
-                mylist,R.layout.myitem,new String[] {"city","code"},
-                new int[] {R.id.textView, R.id.textView2});
+                mylist,R.layout.myitem,new String[] {"city","code","img"},
+                new int[] {R.id.textView, R.id.textView2,R.id.imageView});
         lv.setAdapter(adapter);
     }
 }
